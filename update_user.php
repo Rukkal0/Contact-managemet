81d9327d434009c3ccaba0d7b8eb36d8a5bcd1f2
Rukkal0 <rukalbasnet33@gmail.com>
2024-06-28 16:02:07 +0545

    Add files via upload

--- update_user.php
+++ update_user.php
@@ -1,29 +1,24 @@
 <?php
 require('data_base.php');
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['update'])) {
+  $Student_id= $_POST['id'];
   $username = $_POST['username'];
   $email = $_POST['email'];
   $phone_number = $_POST['phone_number'];
   $Address = $_POST['Address'];
   $note = $_POST['Note'];
-
-
-  $sql = 'INSERT INTO information (Name, Email, Number, Address, Note) VALUES (?, ?, ?, ?, ?)';
-  $stmt = $conn->prepare($sql);
-
-
-  $stmt->bind_param('sssss', $username, $email, $phone_number, $Address, $note);
-
-
-  if ($stmt->execute()) {
-    header('Location: ../code/table.php');
-    exit();
-  } else {
-    echo "Error: " . $stmt->error;
-  }
-
-  $stmt->close();
+ 
+ $query="UPDATE information SET Name=?,Email=?,Number=?,Address=?,Note=? WHERE ID=?";
+ $stmt=$conn->prepare($query);
+ $stmt->bind_param('sssssi',$username,$email,$phone_number,$Address,$note,$Student_id);
+ if($stmt->execute()){
+    header('Location:../code/table.php');
+ }
+ else{
+ echo  $stmt->error;
 }
-
+$stmt->close();
+}
 $conn->close();
+?>
